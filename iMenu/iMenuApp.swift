921650704c78e0e54a4174e0e04baf81490aa7f2
rimas12 <rizky_mashudy@yahoo.co.id>
2022-01-03 14:06:45 +0700

    Initial Commit

---
+++ iMenu/iMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMenuApp.swift
+//  iMenu
+//
+//  Created by Rizky Mashudi on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct iMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
