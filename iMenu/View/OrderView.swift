774687496ddcf744508243851ec64019e58095a1
rimas12 <rizky_mashudy@yahoo.co.id>
2022-01-03 18:41:25 +0700

    First commit

---
+++ iMenu/View/OrderView.swift
@@ -0,0 +1,41 @@
+//
+//  OrderView.swift
+//  iMenu
+//
+//  Created by Rizky Mashudi on 03/01/22.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: CheckOutView()) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView().environmentObject(Order())
+    }
+}
